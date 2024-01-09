710d12c8a70d3b66336ad293226469c5f4e68260
pron1x <lukas98z@web.de>
2024-01-09 11:15:30 +0100

    >Initial commit

---
+++ src/main/java/com/calc/onlinecalc/OnlineCalcApplication.java
@@ -0,0 +1,13 @@
+package com.calc.onlinecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calc/onlinecalc/OnlineCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calc.onlinecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
